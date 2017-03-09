1e25376487b160282f48891fd76a67faefbc2b6f
rawatsumit <rawatsumit115@gmail.com>
2017-03-09 17:26:18 +0530

    4 commit

---
+++ mylibrary/src/main/java/in/demolibrary/com/mylibrary/SumitUpdate.java
@@ -0,0 +1,17 @@
+package in.demolibrary.com.mylibrary;
+
+/**
+ * Created by Localadmin on 3/9/2017.
+ */
+
+import android.util.Log;
+
+public class SumitUpdate {
+
+    private static final String Tag="MY_TAG";
+
+    public static void d(String Message)
+    {
+        Log.d(Tag,Message);
+    }
+}
